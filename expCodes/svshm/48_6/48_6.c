202d8dfff4bbaaa8232cb533a0195f3c5cfbfe51
liver0377 <3435951572@qq.com>
2021-11-26 23:22:24 +0800

    48_6

---
+++ expCodes/svshm/48_6/48_6.c
@@ -0,0 +1,34 @@
+#include<sys/types.h>
+#include<sys/shm.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<errno.h>
+#include"/home/liver0377/tlpi/expCodes/lib/tlpi_hdr.h"
+
+
+int main()
+{
+    struct shminfo info;
+    int maxind, ind, shmid;
+    struct shmid_ds ds;
+
+    if ((maxind = shmctl(0, SHM_INFO, (struct shmid_ds *)&info)) == -1)
+        errExit("shmctl");
+
+    printf("maxind: %d\n\n", maxind);
+    printf("index     id       key      nattch      \n");
+
+    for (ind = 0; ind <= maxind; ++ind)
+    {
+        if ((shmid = shmctl(ind, SHM_STAT, &ds)) == -1)
+        {
+            if (errno != EINVAL && errno != EACCES)
+                errExit("shmctl");
+            continue;
+        }
+
+        printf("%4d %8d 0x%08lx %04d\n", ind, shmid, (long)ds.shm_perm.__key, (int)ds.shm_nattch);
+    }
+
+    return 0;
+}
---
+++ expCodes/svshm/48_6/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -o 48_6.out 48_6.c ~/tlpi/expCodes/lib/error_functions.c
+./48_6.out
